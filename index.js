638dc55926dcd30abcd2481afdc199b2e67fb0a3
Cristiano Coelho <cristianocca@hotmail.com>
2022-05-10 11:12:05 -0300

    set up linting

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { Platform, NativeModules } from 'react-native';
+import {Platform, NativeModules} from 'react-native';
 
-const { GlesChecker } = NativeModules;
+const {GlesChecker} = NativeModules;
 
 // Returns OpenGL ES version as a string
-export default async function getGlesVersion(){
-  if(Platform.OS == 'android'){
+export default async function getGlesVersion() {
+  if (Platform.OS == 'android') {
     return await GlesChecker.getGlesVersion();
   }
   // dummy for now
-  return "3";
+  return '3';
 }
